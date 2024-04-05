a3bbc1d491021e025f14068d723388bd0b2ab742
alfuguo <137233519+alfuguo@users.noreply.github.com>
2024-04-05 18:27:59 -0400

    Update

--- assigment08/src/main/java/Assignment8Application.java
+++ assigment08/src/main/java/Assignment8Application.java
@@ -2,11 +2,12 @@ import java.util.List;
 
 public class Assignment8Application {
     public static void main(String[] args) {
+
         FileService fileService = new FileService();
         List<Integer> allNumbers = fileService.getAllNumbers();
         fileService.countAndPrintoutNumbers(allNumbers);
-    }
 
+    }
 }
 
 
--- assigment08/src/main/java/FileService.java
+++ assigment08/src/main/java/FileService.java
@@ -2,36 +2,30 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 public class FileService {
-    ExecutorService executor = Executors.newCachedThreadPool();;
+    ExecutorService executorService = Executors.newCachedThreadPool();;
     Assignment8 assignment = new Assignment8();
 
     public List<Integer> getAllNumbers() {
         List<CompletableFuture<List<Integer>>> numberTaskFutures = new ArrayList<>();
         for (int i = 0; i < 1000; i++) {
             CompletableFuture<List<Integer>> numberTask =
-                    CompletableFuture.supplyAsync(() -> assignment.getNumbers(), executor);
+                    CompletableFuture.supplyAsync(() -> assignment.getNumbers(), executorService);
             numberTaskFutures.add(numberTask);
         }
         CompletableFuture<Void> allNumberTasksCompleted = CompletableFuture.allOf(numberTaskFutures.toArray(new CompletableFuture[0]));
-
         allNumberTasksCompleted.join();
-
         List<Integer> allNumbers = numberTaskFutures.stream()
                 .flatMap(numberTask -> numberTask.join().stream())
                 .collect(Collectors.toList());
 
-        executor.shutdown();
+        executorService.shutdown();
         return allNumbers;
-
     }
-
-
 
     public void countAndPrintoutNumbers(List<Integer> allNumbers) {
         Map<Integer, Long> countMap = allNumbers.stream()
@@ -42,7 +36,9 @@ public class FileService {
                 .map(entry -> entry.getKey() + "=" + entry.getValue())
                 .collect(Collectors.joining(", "));
 
+        System.out.println("✩░▒▓▆▅▃▂▁A Million Numbers Later▁▂▃▅▆▓▒░✩");
         System.out.println(output);
+
     }
 
 
